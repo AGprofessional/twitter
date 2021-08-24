5f7ece9318e5055bab0c8f414568e84ac0ebdc2a
AGprofessional <12345sunnys@gmail.com>
2021-08-24 01:25:39 -0400

    add nav links and react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div class="container">
+        <div class="row">
+         <div class="col-sm-">
+            <div class="colNav">
+
+              <Link to="/">Home</Link>
+    
+               <Link to="/explore">Explore</Link>
+              <Link to="/notifications">Notifications</Link>
+
+              <Link to="/messages">Messages</Link>
+               <Link to="/bookmarks">Bookmarks</Link>
+
+              <Link to="/lists">Lists</Link>
+    
+             </div>
+          </div>
+          <div class="col-xxl-">
+            Column
+           </div>
+         </div>
+      </div>
+     
+        
+
+        <Switch>
+          <Route path="/explore">
+            <Explore />
+          </Route>
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Explore() {
+  return <h2>Explore</h2>;
 }
 
 export default App;
